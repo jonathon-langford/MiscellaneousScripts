a4347d7621c73320dba89b82edaa660ad72e3f17
Jonathon Langford <jl2117@ic.ac.uk>
2019-12-10 13:50:46 +0100

    proper treatment of roodataset and roodatahists

--- RooFit/WS_Manip/ws_mgg_shifter.py
+++ RooFit/WS_Manip/ws_mgg_shifter.py
@@ -7,7 +7,10 @@ parser = OptionParser()
 parser.add_option("--inputFile", dest="inputFile", default="/vols/cms/es811/FinalFits/ws_ReweighAndNewggHweights/output_GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8_GG2H.root", help="Input file")
 parser.add_option("--inputMass", dest="inputMass", default=125, type='int', help="Input mass")
 parser.add_option("--targetMass", dest="targetMass", default=130, type='int', help="Target mass")
+parser.add_option("--verbose", dest="verbose", default=0, type='int', help="Verbose output")
 (opt,args) = parser.parse_args()
+
+verbose = opt.verbose
 
 def rooiter(x):
   iter = x.iterator()
@@ -22,6 +25,9 @@ if not os.path.exists( opt.inputFile ):
 
 if str(opt.inputMass) not in opt.inputFile: 
   print " --> [ERROR] input file %s does not correspond to input mass (%s). Leaving..."%(opt.inputFile,str(opt.inputMass))
+
+# Calculate shift
+shift = float(opt.inputMass-opt.targetMass)
   
 # Extract input file and workspace
 f = ROOT.TFile( opt.inputFile )
@@ -52,37 +58,76 @@ allData = ws.allData()
 # Loop over datasets and define new shifted datasets
 shifted_datasets = {}
 weight = ROOT.RooRealVar("weight","weight",-100000,1000000)
+m = ROOT.RooRealVar("CMS_hgg_mass","m",100,180)
+
 for d_orig in allData:
 
   # Extract names of original an shifted datasets
   n_orig = d_orig.GetName()
-  n_shift = re.sub(str(opt.inputMass),str(opt.targetMass),n_orig)
-
-  print "%s --> %s"%(n_orig,n_shift)
+  if "RECO" in n_orig:
+    n_orig_split = n_orig.split("RECO")
+    n_shift_split_0 = re.sub(str(opt.inputMass),str(opt.targetMass),n_orig_split[0])
+    n_shift = n_shift_split_0 + "RECO" + n_orig_split[-1]
+  else: n_shift = re.sub(str(opt.inputMass),str(opt.targetMass),n_orig)
+  if verbose: print "%s --> %s"%(n_orig,n_shift)
 
   # Create an empty clone of original dataset
-  shifted_datasets[n_shift] = d_shift = d_orig.emptyClone( n_shift )
+  #shifted_datasets[n_shift] = d_shift = d_orig.emptyClone( n_shift )
+  shifted_datasets[n_shift] = d_orig.emptyClone( n_shift )
 
-  # Loop over entries in original dataset
-  for i in range(d_orig.numEntries()):
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # For RooDataHist
+  if d_orig.ClassName() == 'RooDataHist':
+    for i in range(d_orig.numEntries()):
+      
+      #Clone bin and extract weight
+      p = d_orig.get(i).Clone()
+      weight.setVal(d_orig.weight())
 
-    # Clone datapoint, shift mass and add to new shifted dataset
-    p = d_orig.get(i).Clone()
-    shift = float(opt.inputMass-opt.targetMass)
-    p.setRealValue("CMS_hgg_mass", (d_orig.get(i).getRealValue("CMS_hgg_mass")-shift) )
+      # Extract centre of bin
+      binCentre = p.getRealValue("CMS_hgg_mass")
 
-    # Extract weight
-    weight.setVal(d_orig.weight())
+      # If binCentre +- shift is outside allowed range then skip
+      if( (binCentre-shift) < m.getMin() )|( (binCentre-shift) > m.getMax() ): continue
+      else:
+        # Shift binCentre and set new value
+        p.setRealValue("CMS_hgg_mass", (d_orig.get(i).getRealValue("CMS_hgg_mass")-shift) )
+        # Add to shifted datahist
+        #shifted_datasets[n_shift].add(p,weight.getVal())
+        shifted_datasets[n_shift].add(p,weight.getVal())
 
-    # Add shifted dataset to dictionary
-    shifted_datasets[n_shift].add(p,weight.getVal())
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # For RooDataSet
+  else:
+    # Loop over entries in original dataset
+    for i in range(d_orig.numEntries()):
+
+      # Clone datapoint, with same weight
+      p = d_orig.get(i).Clone()
+      weight.setVal(d_orig.weight())
+
+      # If data point contains Higgs mass then shift
+      if p.contains(m):
+
+        value = p.getRealValue("CMS_hgg_mass")
+
+        # If value - shift is outside allowed range then skip
+        if( (value-shift) < m.getMin() )|( (value-shift) > m.getMax() ): continue
+        else:
+          # Set ot new shifted value
+          p.setRealValue("CMS_hgg_mass", (d_orig.get(i).getRealValue("CMS_hgg_mass")-shift) )
+          # Add to shifted dataset
+          shifted_datasets[n_shift].add(p,weight.getVal())
+    
+      # For pdfWeights: keep same points
+      else: shifted_datasets[n_shift].add(p,weight.getVal())
 
 # Import all shifted datasets to output ws
 for d_shift in shifted_datasets.itervalues(): wsout.imp( d_shift, ROOT.RooFit.RecycleConflictNodes() )
 
 # Configure output file and write output ws
 if not os.path.isdir("./shifted_ws"): os.system("mkdir ./shifted_ws")
-fout_name = re.sub( str(opt.inputMass), str(opt.targetMass), opt.inputFile.split("/")[-1] )
+fout_name = re.sub( "M%s"%str(opt.inputMass), "M%s"%str(opt.targetMass), opt.inputFile.split("/")[-1] )
 fout = ROOT.TFile.Open("./shifted_ws/%s"%fout_name, "RECREATE")
 dir_ws = fout.mkdir("tagsDumper")
 dir_ws.cd()
